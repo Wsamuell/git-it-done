d6097c5ccd5e267306f6c91ffd20e15d646f3abe
samuel Odubamowo <wemsamuel@gmail.com>
2021-04-23 22:53:39 -0700

    complete

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -92,7 +92,7 @@ var displayRepos = function (repos, searchTerm) {
         // check if current repo has issues or not
         if (repos[i].open_issues_count > 0) {
             statusEl.innerHTML =
-                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+                "<i  class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
         } else {
             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
@@ -115,7 +115,7 @@ var getFeaturedRepos = function (language) {
                 displayRepos(data.items, language);
             });
         } else {
-            alert('Error: GitHub User Not Found');
+            alert("Error: " + response.statusText);
         }
     });
 };
